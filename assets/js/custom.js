bfd3732269be237a3485cb8e2b3106a7f5312b50
Tobija Fischer <tobijafischer@gmail.com>
2019-10-29 01:56:27 +0100

    giphy easteregg improvements

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -171,14 +171,16 @@ $(window).on('load', function(){
         clearTimeout(timeout);
         document.body.classList.remove('idle');
         timeout = setTimeout(function(){
-            var xhr = $.get('https://api.giphy.com/v1/gifs/random?api_key=oqQ3Y5EvPTePdLjDXViKnbiUP9FQ7NT6&tag=lebowski&limit=1');
+            var xhr = $.get('https://api.giphy.com/v1/gifs/random?api_key=oqQ3Y5EvPTePdLjDXViKnbiUP9FQ7NT6&tag=the-big-lebowski&limit=1');
             xhr.done(function(data) {
                 console.log(data.data.embed_url);
                 $('#dude-wrapper img').attr('src', data.data.image_original_url);
                 // $('#dude-wrapper').css('z-index', '11');
             })
-            document.body.classList.add('idle');
-        }, 30000);
+            setTimeout(function(){
+                document.body.classList.add('idle');
+            }, 5000);
+        }, 25000);
         }
     }
     getDudeGif();
