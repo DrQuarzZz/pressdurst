3fd3e92bba7e828c34813e4fbe966c20782b3052
Tobija Fischer <tobijafischer@gmail.com>
2019-11-21 16:56:50 +0100

    Cleared some console logs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
                     getImageWrapHeight = getTitleHeight + getMainMesssageHeight,
                     calculatedViewportHeight = window.innerHeight;
                     
-                    console.log(calculatedViewportHeight - getImageWrapHeight);
+                    // console.log(calculatedViewportHeight - getImageWrapHeight);
 
                     $(this).closest('.fullscreen-img-wrap').find('.inner-bg-wrap').css({
                         'height': calculatedViewportHeight - getImageWrapHeight + 'px',
@@ -146,7 +146,7 @@ $(window).on('load', function(){
 
     $('#anmeldung').on('click', function(e){
         e.preventDefault();
-        console.log('well at least that worked');
+        // console.log('well at least that worked');
         // $(this).closest('#anmelden-wrapper').find('#normal-scroll').slideToggle();
         if ( $(this).closest('#anmelden-wrapper').hasClass('anmeldung-open') ) {
             $(this).closest('#anmelden-wrapper').removeClass('anmeldung-open');
@@ -173,7 +173,7 @@ $(window).on('load', function(){
         timeout = setTimeout(function(){
             var xhr = $.get('https://api.giphy.com/v1/gifs/random?api_key=oqQ3Y5EvPTePdLjDXViKnbiUP9FQ7NT6&tag=the-big-lebowski&limit=1');
             xhr.done(function(data) {
-                console.log(data.data.embed_url);
+                // console.log(data.data.embed_url);
                 $('#dude-wrapper img').attr('src', data.data.image_original_url);
                 // $('#dude-wrapper').css('z-index', '11');
             })
